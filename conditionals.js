613cb819ca3f9576c5ca1c0cb7bd03cec2e4521d
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-13 04:19:34 +0000

    added more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -8,7 +8,7 @@ if (raining) {
 
 const temp = 16
 
-if(temp<0) {
+if(temp <0) {
  console.log('make sure to wear a scraf')
 } else if (temp <15){
   console.log ('short sleeve wont cut it!')
@@ -16,3 +16,17 @@ if(temp<0) {
   console.log('short sleeve is fine!')
 }
 console.log('Now you/re ready to go outside');
+
+
+const isCitizen = true;
+const age = 16;
+
+if(isCitizen && age>18) {
+  console.log('youre eligible to vote!')
+} else {
+  console.log('youre too young');
+} 
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
