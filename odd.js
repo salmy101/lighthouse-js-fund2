6b7e2155db149f1804611decd7641a8f99bfa405
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-14 22:03:17 +0000

    made a function to tell if number is odd, if odd will say true

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd = function(num) {
+  return num % 2 !== 0 
+}
+ console.log(isOdd(5));
+
+ let threeIsOdd = isOdd(3)
+ console.log(threeIsOdd);
+
+ let fourIsOdd = isOdd(4)
+ console.log(fourIsOdd);
