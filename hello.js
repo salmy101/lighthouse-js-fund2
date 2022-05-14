1db19ace6f2eb7255af680b9bf316e32ba1b562a
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-05-14 03:38:09 +0000

    looked at the difference betwwen return statement and console.log statements

--- hello.js
+++ hello.js
@@ -5,3 +5,15 @@ const sayHello = function (name) {
 sayHello("salma");
 
 sayHello("bethany")
+
+const sayBye = function (name) {
+  console.log("goodbye" + name);
+}
+sayBye('salma'); /*I get a value when i call the fucntion because 
+A console.log statement will result in some content being displayed in the console. */
+
+const myAge = function(num) {
+  return "my age is " + num;
+}
+myAge(13); //A return statement will not output anything to the console
+console.log(myAge(13)); // I can cnsole.log the value it returns
